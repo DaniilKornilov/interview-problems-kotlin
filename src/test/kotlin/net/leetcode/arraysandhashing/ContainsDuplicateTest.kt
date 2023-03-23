276a52655f809102761dd0e8fd2f86d9f93d56ce
Daniil Kornilov <daniil-kornilov@yandex.ru>
2023-03-24 01:35:00 +0400

    add tests for containsDuplicate

--- src/test/kotlin/net/leetcode/arraysandhashing/ContainsDuplicateTest.kt
+++ src/test/kotlin/net/leetcode/arraysandhashing/ContainsDuplicateTest.kt
@@ -1,4 +1,23 @@
 package net.leetcode.arraysandhashing
 
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.DynamicTest
+import org.junit.jupiter.api.TestFactory
+
 class ContainsDuplicateTest {
+
+    @TestFactory
+    fun testContainsDuplicate(): List<DynamicTest> {
+        return listOf(
+            intArrayOf(1, 2, 3, 1) to true,
+            intArrayOf(1, 2, 3, 4) to false,
+            intArrayOf(1, 1, 1, 3, 3, 4, 3, 2, 4, 2) to true,
+        )
+            .map { (input, expected) ->
+                DynamicTest.dynamicTest("When input array ${input.contentToString()} then $expected") {
+                    assertEquals(expected, containsDuplicate(input))
+                }
+            }
+    }
+
 }
